20ed7dc16d032c785407ad05c1863a34f4e00737
Vini Duarte <85229756+vinizika@users.noreply.github.com>
2024-03-08 18:44:11 -0300

    Adição da opção 6 ao código, em que retorna apenas o comprimento de onda, por enquanto.

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from math import *
+
+print("Escolha uma opção de entrada: ")
+
+print("[1] - Amplitude de Campo Eletrico\n[2] - Amplitude do Campo Magnetico\n[3] - Intensidade da Onda Magnetica\n")
+print("[4] - Frequencia\n[5] - Comprimento da onda\n[6] - Numero de onda\n[7] - Frequencia Angular\n")
+opcao = int(input("Digite a opção desejada: "))
+if opcao < 1 or opcao > 7:
+  print("Opção invalida")
+
+if opcao == 6: 
+  numero_onda = float(input("Digite o numero de onda [rad/m]: "))
+  comprimento_onda = (2 * pi)/numero_onda
+  print("Comprimento de onda: %.2f m\n", comprimento_onda)
+
 
